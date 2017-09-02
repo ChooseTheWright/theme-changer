5bafa4cce8053e512e77793b8339a3a2bc6a0656
Wright <koltw@ogdencity.com>
2017-09-02 12:19:14 -0600

    Completed steps 1-6 and finished all steps

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -8,11 +8,13 @@ export default class FamilyChanger extends Component {
     };
   }
 
-  // componentWillReceiveProps
+  componentWillReceiveProps (props) {
+    this.setState({allowEdit: props.allowEdit});
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" disabled={this.state.allowEdit === 'false'} onChange={(e) => {this.props.update(e.target.value);}}>
         <option value="monospace"> Monospace </option>
         <option value="arial"> Arial </option>
         <option value="courier"> Courier </option>
